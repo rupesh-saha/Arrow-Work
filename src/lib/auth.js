f4ea1b2f2b8b79c337aaf50fac0e02945e6516e4
rupesh-saha <saharupesh291@gmail.com>
2026-06-30 02:53:26 +0600

    jwt bug

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const getDb = () => {
   const client = new MongoClient(process.env.ARROW_URI);
