e60095e621ed4026f634ac46d849308303846657
rupesh-saha <saharupesh291@gmail.com>
2026-06-30 16:10:34 +0600

    proxy fixd

--- src/proxy.js
+++ src/proxy.js
@@ -1,16 +1,14 @@
 import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth"; // Import your auth instance
+import { auth } from "@/lib/auth"; 
 import { headers } from "next/headers";
 
 export async function proxy(request) {
   const { pathname } = request.nextUrl;
 
-  // 1. Use the auth instance to get the session
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  // 2. Logic for protecting dashboard routes
   if (!session) {
     if (pathname.startsWith("/dashboard")) {
       return NextResponse.redirect(new URL("/login", request.url));
@@ -18,7 +16,6 @@ export async function proxy(request) {
     return NextResponse.next();
   }
 
-  // 3. RBAC Redirects
   const role = session.user.role;
   const rolePaths = {
     admin: "/dashboard/admin",
@@ -28,7 +25,9 @@ export async function proxy(request) {
 
   const targetPath = rolePaths[role];
 
-  if (pathname.startsWith("/dashboard") && !pathname.startsWith(targetPath)) {
+  const isSuccessPage = pathname.startsWith("/dashboard/success");
+
+  if (pathname.startsWith("/dashboard") && !pathname.startsWith(targetPath) && !isSuccessPage) {
     return NextResponse.redirect(new URL(targetPath, request.url));
   }
 
